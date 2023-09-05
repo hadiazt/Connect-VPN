dd5b1efce2320aaba46e0ebb10cc29d826a6c1bc
hadi <hadiazari30@gmail.com>
2023-09-05 20:46:04 +0430

    Update routes.ts

--- src/functions/routes.ts
+++ src/functions/routes.ts
@@ -1,6 +1,10 @@
 export { default as Start } from './command/start';
 
 export { default as Home } from './action/home';
-export { default as SearchService } from './action/search';
 export { default as AdminPanel } from './action/panel';
-export { default as Moderation } from './action/moderation'
+export { default as SearchService } from './action/search';
+export { default as Moderation } from './action/moderation';
+
+export { default as AddAdmin } from './action/mod/addAdmin';
+export { default as RemoveAdmin } from './action/mod/remAdmin';
+export { default as ConnectionLimit } from './action/mod/conlimit';
