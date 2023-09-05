87402868dedc6bf0ee455547aec04ac7fcb6d7e6
hadi <hadiazari30@gmail.com>
2023-09-05 23:25:07 +0430

    Create login.ts

---
+++ src/functions/api/login.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { Panel } from '../../../config.json'
+export default async () => {
+    return new Promise((resolve, reject) => {
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: Panel.BaseURL + 'login',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            data: {
+                username: Panel.UserName,
+                password: Panel.Password
+            }
+        };
+        axios.request(config).then((response) => {
+            const token = response.headers['set-cookie'][0].split(';')[0]
+            resolve({ msg: 'عملیات با موفقیت انجام شد', token })
+            console.log();
+        }).catch((error) => {
+            reject({ msg: 'عملیات با خطا مواجه شد\n' + error })
+        });
+    })
+}
+
+
