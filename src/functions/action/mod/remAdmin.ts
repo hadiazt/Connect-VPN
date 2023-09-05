366311570a4c73895680f2616af960fdd5907671
hadi <hadiazari30@gmail.com>
2023-09-05 20:46:16 +0430

    Create remAdmin.ts

---
+++ src/functions/action/mod/remAdmin.ts
@@ -0,0 +1,16 @@
+import { Database } from 'beta.db';
+const db = new Database('./src/data/config.json');
+
+export default async (ctx: any) => {            
+    if (db.get('Admins').includes(String(ctx.update.callback_query.from.id))) {
+        ctx.scene.enter('AdminRQ')
+    } else {
+        ctx.editMessageText('❌ شمادرسترسی ندارید ❌', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: '🏚️ منوی اصلی 🏚️', callback_data: 'Home' }],
+                ]
+            }
+        })
+    }
+}
