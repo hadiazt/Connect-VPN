d597e47cd2f349f72ce0643fdc76a27ad2e8f9ed
hadi <hadiazari30@gmail.com>
2023-09-05 20:46:13 +0430

    Update moderation.ts

--- src/functions/action/moderation.ts
+++ src/functions/action/moderation.ts
@@ -2,9 +2,9 @@ export default async (ctx: { editMessageText: Function }) => {
     ctx.editMessageText('👨🏻‍💻 لطفا گزینه مورد نظر خود را انتخاب کنید.', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: '👤 افزودن ادمین 👤', callback_data: 'AddAdmin' }],
-                [{ text: '👤 حذف ادمین  👤', callback_data: 'RemoveAdmin' }],
-                [{ text: '🏚️ منوی اصلی 🏚️', callback_data: '' }],
+                [{ text: '👤 افزودن ادمین 👤', callback_data: 'AddAdmin' }, { text: '👤 حذف ادمین  👤', callback_data: 'RemoveAdmin' }],
+                [{ text: '🛑 محدودیت اتصال 🛑', callback_data: 'ConnectionLimit' }],
+                [{ text: '🏚️ منوی اصلی 🏚️', callback_data: 'Home' }],
             ]
         }
     })
