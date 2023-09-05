4877388cfd6e15e7a202b5d2417a3c085891a0f2
hadi <hadiazari30@gmail.com>
2023-09-06 00:49:11 +0430

    Update panel.ts

--- src/functions/action/panel.ts
+++ src/functions/action/panel.ts
@@ -1,8 +1,23 @@
 import login from "../api/login"
-
+import { Status } from "../routes"
 export default async (ctx: { editMessageText: Function }) => {
-    login().then((res: { msg: string }) => {
+    login().then((res: { token: string }) => {
+        Status(res.token).then((res: {
+            msg: string, data: {}
+        }) => {
+            console.log(res.data);
+            // ctx.editMessageText(res.msg, {
+            //     reply_markup: {
+            //         inline_keyboard: [
+            //             [{ text: 'CPU Speed', callback_data: 'X' }, { text: 'CPU Core', callback_data: 'X' }, { text: 'CPU Usage', callback_data: 'X' },],
+            //             [{ text: res.data.cpuSpeedMhz, callback_data: 'X' }, { text: res.data.cpuCores, callback_data: 'X' }, { text: res.data.cpu, callback_data: 'X' },],
+            //         ]
+            //     }
+            // })
 
+        }).catch((e: { msg: string }) => {
+            ctx.editMessageText(e.msg)
+        })
     }).catch((e: { msg: string }) => {
         ctx.editMessageText(e.msg)
     })
