a21a75b76d07beb81abecb828e331de9c1290793
hadi <hadiazari30@gmail.com>
2023-09-06 00:49:16 +0430

    Update routes.ts

--- src/functions/routes.ts
+++ src/functions/routes.ts
@@ -3,3 +3,4 @@ export { default as Start } from './command/start';
 export { default as PanelStatus } from './action/panel';
 
 export { default as Login } from './api/login';
+export { default as Status} from './api/status';
