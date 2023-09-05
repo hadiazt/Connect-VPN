2bbc13edf8a713bfdcbb388c518c0fc0227d44b1
hadi <hadiazari30@gmail.com>
2023-09-06 00:49:18 +0430

    Create public.ts

---
+++ src/functions/public.ts
@@ -0,0 +1,23 @@
+export const CPUPercent = (Entry: number) => {
+    const Usage = (Entry.toFixed(1)) + "%";
+    return Usage;
+}
+
+export const CPUSpeed = (Entry: number) => {
+    const SpeedGhz = (Entry / 1000).toFixed(2) + " GHz";
+    return SpeedGhz;
+}
+
+export const ConvertBytes = (bytes: number) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Bytes';
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const formattedSize = parseFloat((bytes / Math.pow(1024, i)).toFixed(2));
+    return `${formattedSize} ${sizes[i]}`;
+}
+
+export const convertSecondsToDays = (time: number) => {
+    const days = Math.round(time / (24 * 60 * 60));
+    return days;
+
+}
