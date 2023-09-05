8c5e1c526e3c0bebf536b6a127b160574f02ca20
hadi <hadiazari30@gmail.com>
2023-09-06 01:24:59 +0430

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { Telegraf } from 'telegraf';
 import { BotToken } from '../config.json';
-import { Start,PanelStatus } from './functions/routes';
+import { Start, PanelStatus } from './functions/routes';
 
 const bot = new Telegraf(BotToken);
 bot.launch();
 
 
 bot.command('start', Start);
-bot.action('PanelStatus', PanelStatus)
+bot.action('PanelStatus', PanelStatus);
