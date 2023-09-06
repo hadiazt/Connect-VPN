150f311a6a8f8224874e452078eb7b11f0d0052e
hadi <hadiazari30@gmail.com>
2023-09-06 19:05:17 +0430

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { Telegraf } from 'telegraf';
 import { BotToken } from '../config.json';
+import { AutoPortChecker } from './functions/public';
 import { Start, Home, PanelStatus, PortStatus, PortsStatus,IPS } from './functions/routes';
 
 const bot = new Telegraf(BotToken);
 bot.launch();
 
-
+AutoPortChecker(bot)
 bot.command('start', Start);
 
 
-bot.action('Home', Home);
 bot.action(/IPS_/, IPS);
+bot.action('Home', Home);
 bot.action(/Port_/, PortStatus);
 bot.action('PortStatus', PortsStatus);
 bot.action('PanelStatus', PanelStatus);
